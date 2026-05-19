b25fba53c04f259898c180ba390de3c7a76557e8
GasparVardanyan <gaspar_pm@proton.me>
2026-05-19 16:05:18 +0400

    simplified compilation and todos

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 # TODOS
 
-* [ ] finish the requires clause section
+* [ ] expand the "Storing values in the inheritance hierarchy" section with
+examples of complex inheritance hierarchies and virtual inheritance
+* [ ] finish the "The requires clause" section
 * [ ] implement big five along with templated constructors
 * [ ] implement std::tie and std::ignore
 * [ ] make constructors constexpr and propogate noexcept through the inheritance
--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo building tuple-in-cpp20.pdf
-pandoc tuple-in-cpp20.md -o tuple-in-cpp20.pdf --pdf-engine=xelatex --from=gfm --wrap=auto --columns=80 -H header.tex --pdf-engine-opt=-shell-escape --lua-filter minted-wrap.lua --no-highlight --toc
-echo building tuple-in-cpp20\ SolarizedDark.pdf
-pandoc tuple-in-cpp20.md -o tuple-in-cpp20\ SolarizedDark.pdf --pdf-engine=xelatex --from=gfm --wrap=auto --columns=80 -H header-solarized-dark.tex --pdf-engine-opt=-shell-escape --lua-filter minted-wrap.lua --no-highlight --toc
-echo building tuple-in-cpp20\ SolarizedLight.pdf
-pandoc tuple-in-cpp20.md -o tuple-in-cpp20\ SolarizedLight.pdf --pdf-engine=xelatex --from=gfm --wrap=auto --columns=80 -H header-solarized-light.tex --pdf-engine-opt=-shell-escape --lua-filter minted-wrap.lua --no-highlight --toc
+pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20.pdf                 --pdf-engine=xelatex  -H  header.tex                  --lua-filter  minted-wrap.lua  --no-highlight
+pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20_SolarizedDark.pdf   --pdf-engine=xelatex  -H  header-solarized-dark.tex   --lua-filter  minted-wrap.lua  --no-highlight
+pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20_SolarizedLight.pdf  --pdf-engine=xelatex  -H  header-solarized-light.tex  --lua-filter  minted-wrap.lua  --no-highlight
