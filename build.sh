dccca3ff20ad360dfe3868f74a3db75c415a37ea
GasparVardanyan <gaspar_pm@proton.me>
2026-05-24 20:29:46 +0400

    fix the build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20.pdf                 --pdf-engine=xelatex  -H  header.tex                  --lua-filter  minted-wrap.lua  --no-highlight
-pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20_SolarizedDark.pdf   --pdf-engine=xelatex  -H  header-solarized-dark.tex   --lua-filter  minted-wrap.lua  --no-highlight
-pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20_SolarizedLight.pdf  --pdf-engine=xelatex  -H  header-solarized-light.tex  --lua-filter  minted-wrap.lua  --no-highlight
+pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20.pdf                 --pdf-engine=xelatex  --from=gfm  -H  header.tex                  --lua-filter  minted-wrap.lua  --no-highlight
+pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20_SolarizedDark.pdf   --pdf-engine=xelatex  --from=gfm  -H  header-solarized-dark.tex   --lua-filter  minted-wrap.lua  --no-highlight
+pandoc  tuple-in-cpp20.md  -o  tuple-in-cpp20_SolarizedLight.pdf  --pdf-engine=xelatex  --from=gfm  -H  header-solarized-light.tex  --lua-filter  minted-wrap.lua  --no-highlight
